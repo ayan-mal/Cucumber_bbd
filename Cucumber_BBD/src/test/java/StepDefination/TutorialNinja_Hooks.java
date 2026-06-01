f1057c006f8383ac99202af25d0c2eaade18eebd
Ayan Mal <ayan.mal201@gmail.com>
2026-06-01 22:53:40 +0530

    new commit

--- Cucumber_BBD/src/test/java/StepDefination/TutorialNinja_Hooks.java
+++ Cucumber_BBD/src/test/java/StepDefination/TutorialNinja_Hooks.java
@@ -24,27 +24,6 @@ public class TutorialNinja_Hooks {
 	  if(driver != null) {
         driver.quit();
   } //for cucumber run
-	/*@Before
-	public void setup() {
-
-	    EdgeOptions options = new EdgeOptions();
-
-	    options.addArguments("--headless");
-	    options.addArguments("--disable-gpu");
-	    options.addArguments("--window-size=1920,1080");
-	    options.addArguments("--remote-debugging-port=9222");
-	    options.addArguments("--remote-allow-origins=*");
-
-	    driver = new EdgeDriver(options);
-
-	    driver.get("https://tutorialsninja.com/demo/");
-	}
-
-	    @After
-	    public void close() {
-	        if (driver != null) {
-	            driver.quit();
-	        }*/
 	 
 }
 }
