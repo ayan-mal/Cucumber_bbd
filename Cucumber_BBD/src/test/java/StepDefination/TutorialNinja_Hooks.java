55784e5858aa17e70d9ae4ec6ac5361f972078cf
Ayan Mal <ayan.mal201@gmail.com>
2026-06-01 20:57:43 +0530

    new commit

--- Cucumber_BBD/src/test/java/StepDefination/TutorialNinja_Hooks.java
+++ Cucumber_BBD/src/test/java/StepDefination/TutorialNinja_Hooks.java
@@ -5,13 +5,14 @@ import io.cucumber.java.Before;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class TutorialNinja_Hooks {
 	public static WebDriver driver;
 	//TutorialNinja_Hooks(WebDriver driver){
     //	this.driver=driver;
    // }
-  @Before // always execute before all test
+/*  @Before // always execute before all test
   public void setup() {
 	  driver = new EdgeDriver();
 	  driver.get("https://tutorialsninja.com/demo/");
@@ -21,7 +22,28 @@ public class TutorialNinja_Hooks {
   public void close() {
 	  if(driver != null) {
         driver.quit();
-  }
+  }*/ //for cucumber run
+	    @Before
+	    public void setup() {
 
+	        EdgeOptions options = new EdgeOptions();
+
+	        options.addArguments("--headless=new");
+	        options.addArguments("--disable-gpu");
+	        options.addArguments("--window-size=1920,1080");
+	        options.addArguments("--no-sandbox");
+	        options.addArguments("--disable-dev-shm-usage");
+
+	        driver = new EdgeDriver(options);
+
+	        driver.get("https://tutorialsninja.com/demo/");
+	    }
+
+	    @After
+	    public void close() {
+	        if (driver != null) {
+	            driver.quit();
+	        }
+	 
 }
 }
